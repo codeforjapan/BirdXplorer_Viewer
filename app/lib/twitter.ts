2f78ac7b5a00945498807ddff651a50e426c654c
sushichan044 <mail@sushichan.live>
2025-03-04 15:53:53 +0900

    feat: link builder from Note

---
+++ app/lib/twitter.ts
@@ -0,0 +1,10 @@
+import type { Note } from "../generated/api/schemas";
+
+export const birdWatchLinkFromNote = (note: Note): string => {
+  return `https://x.com/i/birdwatch/t/${note.postId}`;
+};
+
+export const postLinkFromNote = (note: Note): string => {
+  // X redirects to correct post url regardless of userId, so just specify `i`
+  return `https://x.com/i/status/${note.postId}`;
+};
