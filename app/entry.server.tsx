91076a694982e8ba334423f8f1be175f574ce945
sushichan044 <mail@sushichan.live>
2025-03-10 14:21:53 +0900

    fix: set secure headers

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -15,5 +15,15 @@ export default async function handleRequest(
     routerContext,
     loadContext,
   );
+
+  response.headers.set(
+    "Strict-Transport-Security",
+    "max-age=63072000; includeSubDomains; preload",
+  );
+  response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+  response.headers.set("X-Content-Type-Options", "nosniff");
+  response.headers.set("X-Frame-Options", "DENY");
+  response.headers.set("X-Permitted-Cross-Domain-Policies", "none");
+
   return response;
 }
