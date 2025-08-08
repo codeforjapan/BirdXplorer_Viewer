dc98ddf6ddcf5ea14e9b64c909e92b3b33b79e98
Kentaro Suzuki <71284054+sushichan044@users.noreply.github.com>
2025-08-08 18:11:47 +0900

    Use vitest browser mode (#167)
    * chore: config file を統合する
    
    * chore: nodejs の型定義が抜けていた
    
    * ci: use vitest browser mode instead of jsdom
    
    * opt in to browser mode
    
    * chore(deps): add node types
    
    * ci: setup browser
    
    * ci: split browser test job
    
    * too much escape
    
    * fix eslint rule
    
    * ci: cache on main

--- test/test-react.tsx
+++ test/test-react.tsx
@@ -2,12 +2,12 @@ import "dayjs/locale/ja";
 
 import { MantineProvider } from "@mantine/core";
 import { DatesProvider } from "@mantine/dates";
-import type { RenderOptions } from "@testing-library/react";
-import { render } from "@testing-library/react";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import type { ReactElement } from "react";
 import type React from "react";
+import type { ComponentRenderOptions } from "vitest-browser-react";
+import { render } from "vitest-browser-react";
 
 import { mantineTheme } from "../app/config/mantine";
 
@@ -25,9 +25,7 @@ const wrapper = ({ children }: { children: React.ReactNode }) => {
 
 const customRender = (
   ui: ReactElement,
-  options?: Omit<RenderOptions, "wrapper">,
+  options?: Omit<ComponentRenderOptions, "wrapper">,
 ) => render(ui, { wrapper, ...options });
 
-// eslint-disable-next-line react-refresh/only-export-components
-export * from "@testing-library/react";
 export { customRender as render };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference types="vitest/config" />
+
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import Icons from "unplugin-icons/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { defaultExclude } from "vitest/config";
 
 export default defineConfig({
   plugins: [
@@ -16,4 +20,28 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
+  test: {
+    projects: [
+      {
+        test: {
+          name: "Node",
+          environment: "node",
+          exclude: [...defaultExclude, "**/*.browser.test.{ts,tsx}"],
+        },
+      },
+      {
+        test: {
+          name: "Browser",
+          browser: {
+            enabled: true,
+            provider: "playwright",
+            // https://vitest.dev/guide/browser/playwright
+            instances: [{ browser: "chromium" }],
+            headless: true,
+          },
+          include: ["**/*.browser.test.{ts,tsx}"],
+        },
+      },
+    ],
+  },
 });
