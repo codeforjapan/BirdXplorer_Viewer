2700975665b66ee3e4eac58ad62cb62156accf12
yawn <69970183+yawn-c111@users.noreply.github.com>
2025-11-18 19:43:58 +0900

    feat: update route configuration and iframe source for Kouchou AI project

--- app/routes/test-iframe.tsx
+++ app/routes/test-iframe.tsx
@@ -7,7 +7,7 @@ export default function Test() {
       <iframe 
         height="1200px"
         sandbox="allow-scripts allow-popups allow-forms"
-        src="/kouchou-ai/publish/index.html"
+        src="/kouchou-ai/52c5c1bc-fb89-4aa9-ab67-b35e2f663cf2/index.html"
         title="広聴AI"
         width="100%"
       />
