6871d7eb5185e96c99080a049c2426fa9f802056
ayuki <19406594+ayuki-joto@users.noreply.github.com>
2026-04-09 18:21:12 +0900

    Update _index.tsx for 2026-03

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -145,7 +145,7 @@ export default function Index({ loaderData }: Route.ComponentProps) {
           />
           <AutoResizeIframe
             sandbox="allow-scripts allow-popups allow-forms allow-same-origin"
-            src="/kouchou-ai/2026/02/4ebb7806-50c9-458c-a56b-7969c1e272e4/index.html"
+            src="/kouchou-ai/2026/03/46c91940-d212-4655-a4a9-3b090372d388/index.html"
             title="広聴AI"
           />
         </Stack>
